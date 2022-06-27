8d4c349e37145a23107771e36ff174c862b127d3
bill0x2A <billysmith1998@live.com>
2022-06-27 16:05:02 +0100

    Login functional

--- src/components/SignInWithGithub.tsx
+++ src/components/SignInWithGithub.tsx
@@ -1,15 +1,45 @@
 import * as React from 'react';
-import { Button } from '@chakra-ui/react';
-import { useFirebase } from '../context/firebase';
-import firebaseAuth from 'firebase/auth';
-import { auth as authUI } from 'firebaseui';
+import { Button, Center, Image, Text } from '@chakra-ui/react';
+import { auth, provider } from '../config/firebase';
+import { signInWithPopup } from 'firebase/auth';
+
+interface User {
+    displayName: string;
+    imageUrl: string;
+}
 
 const SignInWithGithub: React.FC = () => {
-    const firebaseApp = useFirebase();
-    const ui = new authUI.AuthUI(firebaseAuth.auth())
-    const signIn = () => {
 
+    const [user, setUser] = React.useState<User>();
+
+    const signIn = () => {
+        signInWithPopup(auth, provider)
+            .then(res => {
+                if (res
+                        && res.user
+                        && res.user.displayName
+                        && res.user.photoURL) {
+                    setUser({
+                        displayName: res.user.displayName,
+                        imageUrl: res.user.photoURL,
+                    })
+                }
+            })
+            .catch(alert);
     };
 
-    return <Button onClick={signIn}>Sign In With Github</Button>
+    if(user) {
+        return <Center>
+            <Image
+                borderRadius={'1000px'}
+                width={'50px'}
+                marginRight={'10px'}
+                src={user.imageUrl}></Image>
+            <Text>{user.displayName}</Text>
+        </Center>;
+    } else {
+        return <Button onClick={signIn}>Sign In With Github</Button>
+    }
 }
+
+export default SignInWithGithub;
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GithubAuthProvider } from 'firebase/auth';
+const { FIREBASE_API_KEY } = process.env;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: FIREBASE_API_KEY,
   authDomain: "hackittogether-3e68b.firebaseapp.com",
   projectId: "hackittogether-3e68b",
   storageBucket: "hackittogether-3e68b.appspot.com",
@@ -15,5 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = app.auth();
-export const provider = app.auth.GithubAuthProvider();
+export const auth = getAuth(app);
+export const provider = new GithubAuthProvider();
+provider.addScope('repo');
